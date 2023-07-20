f9f1c5c2cebf27985038f94505aff55a9bda06f4
OrangeBai <jzp0306@gmail.com>
2023-07-21 01:28:10 +0800

    change project name

--- bash/compare_act_server.sh
+++ bash/compare_act_server.sh
@@ -1,9 +1,9 @@
 for net in vgg16 resnet18; do
   for act in sigmoid tanh relu selu; do
-    qsub train.sh --dataset cifar10 --project EAI/comp_act --net ${net} --act {act} --name ${net}_${act}_bn_init
-    qsub train.sh --dataset cifar10 --project EAI/comp_act --net ${net} --act {act} --name ${net}_${act}_init --no_bn
-    qsub train.sh --dataset cifar10 --project EAI/comp_act --net ${net} --act {act} --name ${net}_${act}_bn --no_init
-    qsub train.sh --dataset cifar10 --project EAI/comp_act --net ${net} --act {act} --name ${net}_${act} --no_bn --no_init
+    qsub train.sh --dataset cifar10 --project EAI_comp_act --net ${net} --act {act} --name ${net}_${act}_bn_init
+    qsub train.sh --dataset cifar10 --project EAI_comp_act --net ${net} --act {act} --name ${net}_${act}_init --no_bn
+    qsub train.sh --dataset cifar10 --project EAI_comp_act --net ${net} --act {act} --name ${net}_${act}_bn --no_init
+    qsub train.sh --dataset cifar10 --project EAI_comp_act --net ${net} --act {act} --name ${net}_${act} --no_bn --no_init
     done
 done
 
