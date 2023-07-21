0387be4a58d8bc5670d0d50db610e8d9bf2f1301
OrangeBai <jzp0306@gmail.com>
2023-07-21 10:05:37 +0800

    change exp settings

--- bash/compare_act_server.sh
+++ bash/compare_act_server.sh
@@ -1,4 +1,4 @@
-for net in vgg16 resnet18; do
+for net in vgg16 resnet34; do
   for act in sigmoid tanh relu selu; do
     qsub train.sh --dataset cifar10 --project EAI_comp_act --net ${net} --act ${act} --name ${net}_${act}_bn_init
     qsub train.sh --dataset cifar10 --project EAI_comp_act --net ${net} --act ${act} --name ${net}_${act}_init --no_bn
